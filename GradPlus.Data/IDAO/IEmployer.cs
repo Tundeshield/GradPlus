8b19dd46d1746edc402e910dae68d5f481991768
Tunde <tundeshield@gmail.com>
2022-07-13 15:52:45 +0100

    Created Employer Interface methods.

---
+++ GradPlus.Data/IDAO/IEmployer.cs
@@ -0,0 +1,20 @@
+﻿using GradPlus.Data.Models.Domain;
+using GradPlus.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradPlus.Data.IDAO
+{
+    public interface IEmployer
+    {
+        void CreateEmployer(Employer employer, GradPlusContext context);
+        Employer GetById(int employerId, GradPlusContext context);
+        void UpdateEmployer(Employer employer, GradPlusContext context);
+        void DeleteEmployer(int employerId, GradPlusContext context);
+        IList<Employer> GetAll();
+        IList<Job> GetEmployerJobs(int employerId);
+    }
+}
