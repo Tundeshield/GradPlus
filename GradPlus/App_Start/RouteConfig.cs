103dd0d0bea302c9a8c2acd1de9db9bc26ad5880
Tunde <tundeshield@gmail.com>
2022-07-13 11:09:15 +0100

    Route setup

--- GradPlus/App_Start/RouteConfig.cs
+++ GradPlus/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace GradPlus
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               name: "Admins",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Admin", action = "GetAdmins", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
